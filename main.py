8be6b8605c597257f0fde8b69a203589506d8c00
Youssef Atef <youssefatef007@gmail.com>
2025-05-01 20:32:45 +0300

    dvc pipeline

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import pandas as pd
-import sys
+import numpy as np
+import dvc.api
 from src.training.preprocess import preprocess_data
 from src.training.train import train_data
 from src.training.evaluate import evaluate
-from hydra.utils import instantiate
-import hydra
-from omegaconf import DictConfig
 
-@hydra.main(config_path="configs", config_name="config", version_base=None)
-def main(cfg: DictConfig):
+def main():
+    params = dvc.api.params_show()
     df = pd.read_csv("data/raw/train.csv")
-    
+
     X_train, X_test, y_train, y_test = preprocess_data(
         df,
-        encoder_cfg=cfg.pipeline.encoder,
-        encoder_name=cfg.pipeline.encoder_name
+        encoder_name=params["encoder_name"]
     )
-    
-    model = train_data(X_train, y_train,cfg.pipeline.model)
+
+    model = train_data(X_train, y_train, params["model"])
     print(evaluate(X_test, y_test, model))
 
 if __name__ == "__main__":
--- src/training/preprocess.py
+++ src/training/preprocess.py
@@ -3,24 +3,31 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 import pickle
-from hydra.utils import instantiate
+import dvc.api
 
-def preprocess_data(df, encoder_cfg, encoder_name):
-    df = df.drop(["PassengerId","Name"], axis=1)
-    df['Age'] = df['Age'].fillna(-1)
-    df = df.fillna('Missing')
+
+def preprocess_data(df, encoder_name):
+    df = df.drop(["PassengerId", "Name"], axis=1)
+    df["Age"] = df["Age"].fillna(-1)
+    df = df.fillna("Missing")
+
     cats = df.select_dtypes(include=["object"]).columns
-    encoder = instantiate(encoder_cfg)
+
+    encoder = OrdinalEncoder()
+
     df[cats] = encoder.fit_transform(df[cats])
+
     with open(f"models/{encoder_name}.pkl", "wb") as f:
         pickle.dump(encoder, f)
+
     X = df.drop("Survived", axis=1)
     y = df["Survived"]
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
     X_train.to_csv("data/processed/X_train.csv", index=False)
     X_test.to_csv("data/processed/X_test.csv", index=False)
-    with open("data/processed/y_train.npy", "w") as f:
-        y_train.to_csv(f, index=False)
-    with open("data/processed/y_test.npy", "w") as f:
-        y_test.to_csv(f, index=False)
+    np.save("data/processed/y_train.npy", y_train)
+    np.save("data/processed/y_test.npy", y_test)
+
     return X_train, X_test, y_train, y_test
